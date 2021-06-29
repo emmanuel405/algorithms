8f4aebc9306d185bbe5a287b849daa086ef376d4
emmanuel405 <57897803+emmanuel405@users.noreply.github.com>
2021-06-29 16:57:21 +0300

    Update SubSumMax.java

--- SubSumMax.java
+++ SubSumMax.java
@@ -4,83 +4,71 @@ public class SubSumMax{
   /**
    * Find the largest sub-matrix of all the sub-matrices
    */
-  public static int SumMax(int [][] mat){
-    int max_sum = mat[0][0], rows = mat.length, cols = mat[0].length, best = 0;
-    int tmp[] = new int[cols];
-    for(int i=0; i<cols; i++){
-      Array2Zero(tmp); // tmp[cls] <-- 0
-      
-      for(int j=0; j<cols; j++){
-        for(int k=0; k<rows; k++){
-          tmp[k] += mat[k][j];
-          best = Best(tmp);
-        }
-        if(best > max_sum)
-          max_sum = best;
-      }
-      
-    }
-    return max_sum;
-  }
-  
-  private static void Array2Zero(int arr[]){
+	public static int[] SumMax(int [][] mat){
+		int max_sum = mat[0][0], rows = mat.length, cols = mat[0].length, best[] = new int[3];
+		int startRow = 0, endRow = 0, startCol = 0 , endCol = 0, tmpRow = 0;
+		int tmp[] = new int[cols];
+		for(int i=0; i<cols; i++){
+			Array2Zero(tmp); // tmp[cls] <-- 0
+
+			for(int j=0; j<cols; j++){
+				for(int k=i; k<rows; k++) {
+					tmp[k] += mat[k][j];
+					best = Best(tmp);
+				}
+				if(best[2] > max_sum){
+					max_sum = best[2];
+					startRow = best[0];
+					endRow = best[1];
+					startCol = i;
+					endCol = j;
+				}
+			}
+
+		}
+		int[] res = {startRow, endRow, startCol, endCol, max_sum};
+		return res;
+	}
+	
+	private static int[] Best(int [] a) {
+		int bestStart = 0, bestEnd = a.length;
+		int max = 0, tempMax = 0, tempStart = 0;
+		int ans [] = new int [3];
+
+		for (int i = 0; i < a.length; i++) {
+			tempMax += a[i];
+			if((tempMax > max) ||
+					(tempMax == max && i - tempStart < bestEnd - bestStart)){
+				max = tempMax;
+				bestStart = tempStart;
+				bestEnd = i;
+			}
+			if(tempMax < 0){
+				tempMax = 0;
+				tempStart = i + 1;
+			}
+		}
+
+		ans[0] = bestStart;
+		ans[1] = bestEnd;
+		ans[2] = max;
+		return ans;
+	}	
+
+	private static void Array2Zero(int arr[]){
 		for(int i=0; i<arr.length; i++)
 			arr[i]=0;
 	}
 
-    private static int Best(int arr[]){
-      int best_max = 0, tmp_max = 0;
-      for(int i=0; i<arr.length; i++){
-        tmp_max += arr[i];
-        if(tmp_max > best_max)
-          best_max = tmp_max;
-        if(tmp_max < 0)
-          tmp_max = 0;
-      }
-      return best_max;
-    }
-  }
-  
- /**
-	 * @param arr, k
-	 * Finding a continuous sub-section with a length of k
-	 * Take the first K numbers and put into 'tmp' and calculate the sum of the numbers
-	 * We take out the most early number and insert the next member in the original array.
-	 * As well as the end of the entire array - and find the maximum required
-	 * 
-	 * @return max
-	 */
-	public static int SubMax(int arr[], int k){
-		int tmp[] = new int[k];
-		int max = 0;
-		for(int i = 0; i < k; i++) {
-			tmp[i] = arr[i];
-			max += arr[i];
-		}
+	public static void main(String[] args) {
+		int[][] mat = {
+				{2, 5 ,  -6, 1},
+				{1,-12, -10,-1},
+				{1, -7 ,-1, -1},
+				{10,61, -1, -1}
+		};
+		int res[] = SumMax(mat);
+		System.out.println(Arrays.toString(res));
 
-		int max_tmp = max;
-		for(int i = k; i < arr.length; i++){
-			max_tmp = max_tmp + arr[i] - tmp[i % k];
-			tmp[i % k] = arr[i];
-			if(max_tmp > max)
-				max = max_tmp;
-		}
-		return max;
 	}
-  
- /* 
-  public static void main(String[] args) {
-
-    int arr[] = {1,4,2,10,2,3,1,0,20};
-    int ans = SubMax(arr, 4);
-    System.out.println(ans);
-
-    int[][] matrix = {{2,1,-3,-4,5},
-                    {0,6,3,4,1},
-                    {2,-2,-1,4,-5},
-                    {-3,3,1,0,3}};
-    System.out.println(SumMax(matrix));
-  
-  }
- */
 }
